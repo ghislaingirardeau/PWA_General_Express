0be835a6dc9c35680a4d6a891ca3dc6ecfdf291b
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-12-05 18:04:40 +0100

    deploy

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Express on Vercel'));
+app.get('/', (req, res) => res.send('Express on Vercel start deploy'));
 
 app.listen(8080, () => console.log('Server ready on port 8080.'));
 
