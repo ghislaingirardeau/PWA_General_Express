8f4472e8affec89fb60d9b6594c86b2f1c3d8891
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-12-05 18:27:54 +0100

    add header

--- api/index.js
+++ api/index.js
@@ -10,6 +10,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  next();
+});
+
 const publicVapidKey = process.env.TUTO_SW_PUBLIC;
 const privateVapidKey = process.env.TUTO_SW_PRIVATE;
 
