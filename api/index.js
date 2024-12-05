6e4bb1f4059c1ab270a979779a9001bd39ce60c6
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-12-05 18:31:33 +0100

    remove auth

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    'Origin, X-Requested-With, Content, Accept, Content-Type'
   );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   next();
