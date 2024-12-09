251da578e2d2748497b856c7fe20af873b5af3e5
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-12-09 12:35:23 +0100

    remove headers

--- api/index.js
+++ api/index.js
@@ -10,28 +10,10 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  next();
-});
 
 const publicVapidKey = process.env.TUTO_SW_PUBLIC;
 const privateVapidKey = process.env.TUTO_SW_PRIVATE;
@@ -51,7 +33,7 @@ app.post('/api/save-subscription', (req, res) => {
   // IF ALL USERS ALLOWED NOTIFICATION => ALL GOT THE NOTIFICATION MESSAGE
   // STORE THE SUBSCRIPTION INSIDE DB
   const subscription = req.body;
-  console.log(subscription);
+  console.log('subscription', subscription);
   subDatabse.push(subscription);
   // SEND A RESPONSE TO USER THAT HE IS CONNECTED TO THE SERVICE WORKER
   res.json({ status: 'Success', message: 'Subscription saved!' });
@@ -59,8 +41,8 @@ app.post('/api/save-subscription', (req, res) => {
 
 app.get('/api/send-notification', (req, res) => {
   // EVERY TIME, THE ROUTE IS CALLED, A NOTIFICATION WILL BE SEND TO THE USER WITH THE MESSAGE ABOVE
-  console.log(subDatabse);
-  webPush.sendNotification(subDatabse[subDatabse.length - 1], 'Hello world');
+  console.log('send notification');
+  webPush.sendNotification(subDatabse[0], 'Hello world');
   res.json({ statue: 'Success', message: 'Message sent to push' });
 });
 
